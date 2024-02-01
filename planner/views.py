060fe3a5e128d6cb338719f887a8f39d1646ff8a
Poom Khorchitmate <poom.khor@gmail.com>
2024-02-01 15:03:11 +0700

    add views: add templates

--- planner/views.py
+++ planner/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
     return render(request, 'planner/home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def holidays_list(request):
+    holidays = Holiday.objects.all()
+    return render(request, 'planner/holidays_list.html', { 'holidays': holidays})
+
+def planner_dashboard(request):
+    pass
+
+def holidays_detail(request):
+    pass
+
+def destinations_detail(request):
+    pass
+
+def itinerary_detail(request):
+    pass
