2bb0bfc7e0e98c012cef09131589ff3c54cd1f49
Dominic Spinks <dominic.spinks@outlook.com>
2024-02-03 17:28:26 +1100

    Add blog post edit and delete functionality

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_home, name='blog-home'),
-    path('blogs/', views.blog_list, name='blog-list'),
+    path('blog/', views.post_list, name='post-list'),
+    path('blog/<int:post_id>', views.post_details, name='post-details'),
+    path('blog/new', views.PostCreate.as_view(), name='post-create'),
+    path('blog/<int:pk>/delete/', views.PostDelete.as_view(), name='post-delete'),
+    path('blog/<int:pk>/update/', views.PostUpdate.as_view(), name='post-update'),
 ]
