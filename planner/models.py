17a6bb00d84fa00710143aa24423a44c8d762dce
Dominic Spinks <dominic.spinks@outlook.com>
2024-02-09 22:11:31 +1100

    Update styles in holiday detail and destination detail pages; Set destination description to optional

--- planner/models.py
+++ planner/models.py
@@ -21,7 +21,7 @@ class Destination(models.Model):
     location = models.CharField(max_length=100)
     start_date = models.DateField()
     end_date = models.DateField()
-    description=models.TextField(max_length=250)
+    description=models.TextField(max_length=250, blank=True, default='')
 
     def __str__(self):
         return self.location
