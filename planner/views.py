1b7a1b4d1c3b580f8935a4804ede391f8a6e2f84
Dominic Spinks <dominic.spinks@outlook.com>
2024-02-10 14:19:13 +1100

    Add form for accommodations

--- planner/views.py
+++ planner/views.py
@@ -14,6 +14,7 @@ from django.urls import reverse_lazy
 
 # About page
 def about(request):
+
     return render(request, 'about.html')
 
 # Dashboard
@@ -21,22 +22,38 @@ def about(request):
 def planner_dashboard(request):
     holidays = Holiday.objects.filter(user=request.user).order_by('-start_date')[:3]
     posts = Post.objects.filter(user=request.user).order_by('-create_date')
+
     return render(request, 'planner/dashboard.html', { 'holidays': holidays, 'posts': posts })
 
 ## Holiday pages ##
-# List of users holidays
+# List of user's holidays
 @login_required
 def holidays_list(request):
     holidays = Holiday.objects.filter(user=request.user)
+
+    # Get holiday form for add holiday modal view
     holiday_form = HolidayForm()
-    return render(request, 'planner/holidays_list.html', { 'holidays': holidays, 'holiday_form': holiday_form })
+
+    return render(
+        request,
+        'planner/holidays_list.html',
+        {
+            'holidays': holidays,
+            'holiday_form': holiday_form
+        }
+    )
 
 # Details of a single holiday
 @login_required
 def holidays_detail(request, pk):
     holiday = Holiday.objects.get(id=pk)
+
+    # Get holiday form for edit holiday modal view
     holiday_form = HolidayForm(instance=holiday)
+
+    # Get destination form for add destination modal view
     destination_form = DestinationForm()
+
     return render(
         request,
         'planner/holidays_detail.html',
@@ -47,14 +64,17 @@ def holidays_detail(request, pk):
         }
     )
 
+# CBV to handle requests for adding a holiday
 class HolidayCreate(LoginRequiredMixin, CreateView):
     model = Holiday
     fields = ['name', 'start_date', 'end_date']
 
     def form_valid(self, form):
         form.instance.user = self.request.user
+
         return super().form_valid(form)
 
+# CBV to handle requests for updating a holiday
 class HolidayUpdate(LoginRequiredMixin, UpdateView):
     model = Holiday
     fields = ['name', 'start_date', 'end_date']
@@ -66,6 +86,7 @@ class HolidayUpdate(LoginRequiredMixin, UpdateView):
 
         return context
 
+# CBV to handle delete confirmation and deleting holiday from db
 class HolidayDelete(LoginRequiredMixin, DeleteView):
     model = Holiday
     success_url = '/holidays'
@@ -81,6 +102,7 @@ def destinations_detail(request, holiday_id, destination_id):
 
     # Create form for editing the destination details
     destination_form = DestinationForm(instance=destination)
+
     return render(
         request,
         'planner/destination_detail.html',
@@ -91,15 +113,19 @@ def destinations_detail(request, holiday_id, destination_id):
         }
     )
 
+# Handle requests for adding a new holiday
 @login_required
 def destination_create(request, holiday_id):
     form = DestinationForm(request.POST)
+
     if form.is_valid():
         new_destination = form.save(commit=False)
         new_destination.holiday_id = holiday_id
         new_destination.save()
+
     return redirect('holiday-detail',pk=holiday_id)
 
+# CBV to handle updating a destination
 class DestinationUpdate(LoginRequiredMixin, UpdateView):
     model = Destination
     fields = ['location', 'start_date', 'end_date', 'description']
@@ -111,15 +137,17 @@ class DestinationUpdate(LoginRequiredMixin, UpdateView):
 
         return context
 
+# CBV to handle delete confirmation and deleting a destination from db
 class DestinationDelete(LoginRequiredMixin, DeleteView):
     model = Destination
 
     def get_success_url(self):
         holiday_id = self.object.holiday.id
+
         return reverse_lazy('holiday-detail', kwargs={'pk': holiday_id})
 
-
-
+## Itinerary views ##
+# Detail view for a specific itinerary item
 @login_required
 def itinerary_detail(request, destination_id, itinerary_id):
     itinerary = Itinerary.objects.get(id=itinerary_id)
@@ -138,6 +166,7 @@ def itinerary_detail(request, destination_id, itinerary_id):
         }
     )
 
+# CBV to handle requests for adding an itinerary item
 class ItinCreate(LoginRequiredMixin, CreateView):
     model = Itinerary
     fields = ['start_date', 'end_date', 'description']
@@ -146,13 +175,16 @@ class ItinCreate(LoginRequiredMixin, CreateView):
         destination_id = self.kwargs.get('destination_id')
         destination = Destination.objects.get(id=destination_id)
         form.instance.destination = destination
+
         return super().form_valid(form)
 
     def get_success_url(self):
         destination_id = self.object.destination.id
         holiday_id = self.object.destination.holiday.id
+
         return reverse_lazy('destination-detail', kwargs={'holiday_id': holiday_id, 'destination_id': destination_id})
 
+# CBV to handle requests for updating an itinery item
 class ItinUpdate(LoginRequiredMixin, UpdateView):
     model = Itinerary
     fields = ['start_date', 'end_date', 'description']
@@ -163,14 +195,18 @@ class ItinUpdate(LoginRequiredMixin, UpdateView):
 
         return context
 
+# CBV to handle delete confirmation and deleting an itinerary from db
 class ItinDelete(LoginRequiredMixin, DeleteView):
     model = Itinerary
 
     def get_success_url(self):
         destination_id = self.object.destination.id
         holiday_id = self.object.destination.holiday.id
+
         return reverse_lazy('destination-detail', kwargs={'holiday_id': holiday_id, 'destination_id': destination_id})
 
+## Accommodations ##
+# CBV to handle requests for adding a new accommodation item
 class AccomCreate(LoginRequiredMixin, CreateView):
     model = Accommodation
     fields = ['accom_name', 'accom_type']
@@ -179,17 +215,21 @@ class AccomCreate(LoginRequiredMixin, CreateView):
         itinerary_id = self.kwargs.get('itinerary_id')
         itinerary = Itinerary.objects.get(id=itinerary_id)
         form.instance.itinerary = itinerary
+
         return super().form_valid(form)
 
     def get_success_url(self):
         destination_id = self.object.itinerary.destination.id
         holiday_id = self.object.itinerary.destination.holiday.id
+
         return reverse_lazy('destination-detail', kwargs={'holiday_id': holiday_id, 'destination_id': destination_id})
 
+# CBV to handle requests for updating an accommodation item
 class AccomUpdate(LoginRequiredMixin, UpdateView):
     model = Accommodation
     fields = ['accom_name', 'accom_type']
 
+# CBV to handle delete confirmation
 class AccomDelete(LoginRequiredMixin, DeleteView):
     model = Accommodation
 
@@ -198,6 +238,7 @@ class AccomDelete(LoginRequiredMixin, DeleteView):
         itinerary_id = self.object.itinerary.id
         return reverse_lazy('destination-detail', kwargs={'destination_id': destination_id, 'itinerary_id': itinerary_id})
 
+# Handle signing up new users
 def signup(request):
   error_message = ''
   if request.method == 'POST':
